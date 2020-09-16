2906d587a0beaab16b4adad37d7cc37709b5040e
Tim Eschmann <timeschmann@Tims-iMac.local>
2020-09-16 20:34:12 +0200

    ...

--- simulation.hpp
+++ simulation.hpp
@@ -35,7 +35,7 @@
  * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
  * DEALINGS IN THE SOFTWARE.
  *
-/*****************************************************************************
+/*****************************************************************************/
 //  This software incorporates the Armadillo C++ Library
 //  Armadillo C++ Linear Algebra Library
 //  Copyright 2008-2020 Conrad Sanderson (http://conradsanderson.id.au)
